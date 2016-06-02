73e3c73bfa5f2c05043d320194eb2dad03aa42a8
Matt Rasband <matt.rasband@gmail.com>
2016-06-02 10:45:23 -0400

    bump version to fix issue with long_description

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from distutils.core import setup
 
-with open('README.md') as f:
-    long_description = f.read()
-
 
 setup(name='drudge_parser',
-      version='2.0',
+      version='2.1',
       description='Naive parser for the Drudge Report',
-      long_description=long_description,
       author='Matt Rasband',
       author_email='matt.rasband@gmail.com',
       maintainer='Matt Rasband',
       maintainer_email='matt.rasband@gmail.com',
       license='MIT',
       url='https://github.com/mrasband/drudge_parser',
-      download_url='https://github.com/mrasband/drudge_parser/archive/v2.0.tar.gz',
+      download_url='https://github.com/mrasband/drudge_parser/archive/v2.1.tar.gz',
       keywords=['parsing', 'webscraping', 'drudge', 'drudgereport'],
       py_modules=['drudge_parser'],
       classifiers=[
