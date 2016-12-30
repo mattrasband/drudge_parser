ca791de5d91163986afc564e4a3c57839eee5341
Matt Rasband <matt.rasband@gmail.com>
2016-12-30 11:12:01 -0500

    Test fix
    * Updates test page to current, with the new blacklists
    * 3.6 as the 3.x interpreter (tox and travis)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -9,8 +9,7 @@ static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 
 
 @pytest.mark.parametrize('filename, top_count, main_count, col1_count, col2_count, col3_count', (
-    (os.path.join(static_dir, 'page01.html'), 13, 1, 12, 30, 11),
-    (os.path.join(static_dir, 'page_2016_06_01.html'), 6, 1, 13, 19, 14),
+    (os.path.join(static_dir, 'page_2016_12_30.html'), 4, 3, 14, 15, 13),
 ))
 def test_parse(filename, top_count, main_count, col1_count, col2_count,
                col3_count):
